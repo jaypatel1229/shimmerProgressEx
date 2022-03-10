3a84f909a3090a0266e703159f9ab7557a0fc828
jay.patel <jay.patel@1R-80>
2022-03-10 15:39:38 +0530

    Add project files.

---
+++ CircleProgressBar/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Supercharge;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CircleProgressBar
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {       
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var shimmerLayout = FindViewById<ShimmerLayout>(Resource.Id.shimmer_layout);
+            shimmerLayout.StartShimmerAnimation();
+                     
+        }
+    }
+}
